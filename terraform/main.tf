1f29f95f35fd8971897772071ce2d0686005b45d
ObrejanAlin <alin.obrejan@student.unitbv.ro>
2025-07-24 00:05:03 +0300

    added count

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,33 +3,34 @@ locals {
 }
 
 #variable "client_id" {
- #   type = string
-  #  sensitive = true
+#    type = string
+#    sensitive = true
 #}
 
 #variable "client_secret" {
- #   type = string
-  #  sensitive = true
+#    type = string
+#    sensitive = true
 #}
 
 #variable "tenant_id" {
- #   type = string
-  #  sensitive = true
+#    type = string
+#    sensitive = true
 #}
 
 #variable "subscription_id" {
- #   type = string
-  #  sensitive = true
+#    type = string
+#    sensitive = true
 #}
 provider "azurerm" {
   features {}
- # client_id       = var.client_id
-  #client_secret   = var.client_secret
-  #tenant_id       = var.tenant_id
-  #subscription_id = var.subscription_id
+#  client_id       = var.client_id
+#  client_secret   = var.client_secret
+#  tenant_id       = var.tenant_id
+#  subscription_id = var.subscription_id
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${local.prefix}-rg"
+  count = 2
+  name     = "${local.prefix}-rg-${count.index}"
   location = "West Europe"
 }
